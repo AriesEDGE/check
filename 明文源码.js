ae89ec1b4623d08b4b0565c90b757a7231571482
Aries <96482448+AriesEDGE@users.noreply.github.com>
2024-12-23 01:06:23 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = 'cefffd1d-cc58-4560-b4d1-44f1af528f30';
+let proxyIP = 'proxyip.jp.fxxk.dedyn.io';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
